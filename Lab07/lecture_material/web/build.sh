a533533c90de5f2401354348787978581c7ce3cd
Pei-Han Jiang <peihan945@gmail.com>
2026-04-29 17:08:29 +0200

    move generated files to public/

--- Lab07/lecture_material/web/build.sh
+++ Lab07/lecture_material/web/build.sh
@@ -6,6 +6,8 @@ echo "<p>Version:" >version.inc.html
 git rev-parse --short HEAD >>version.inc.html 2>/dev/null || echo "unkown" >>version.inc.html
 echo "</p>" >>version.inc.html
 
-pandoc --template template.html -A version.inc.html index.md >index.html
-pandoc --template template.html -A version.inc.html rules.md >rules.html
+pandoc --template template.html -A version.inc.html index.md >public/index.html
+pandoc --template template.html -A version.inc.html rules.md >public/rules.html
 ./table.py <score.csv | pandoc --template template.html --metadata title="Score" -A version.inc.html - >score.html
+cp main.css public/
+
