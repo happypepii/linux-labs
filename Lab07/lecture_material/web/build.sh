159d9fc7d63f9e258929dec326c3b524f582b330
Pei-Han Jiang <peihan945@gmail.com>
2026-04-29 21:36:06 +0200

    add default value for variables that might be empty

--- Lab07/lecture_material/web/build.sh
+++ Lab07/lecture_material/web/build.sh
@@ -3,11 +3,16 @@
 set -ueo pipefail
 
 html_dir="public"
+verbose=false
 
+test "${1:-none}" == "--verbose" && verbose=true
+
+$verbose && echo "Reading current version..." >&2
 echo "<p>Version:" >version.inc.html
 git rev-parse --short HEAD >>version.inc.html 2>/dev/null || echo "unkown" >>version.inc.html
 echo "</p>" >>version.inc.html
 
+$verbose && echo "Generating HTML ..." >&2
 pandoc --template template.html -A version.inc.html index.md >"$html_dir/index.html"
 pandoc --template template.html -A version.inc.html rules.md >"$html_dir/rules.html"
 TABLE_CAPTION="Scoring table" ./table.py <score.csv | pandoc --template template.html --metadata title="Score" -A version.inc.html - >"$html_dir/score.html"
