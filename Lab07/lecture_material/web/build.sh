139f78ee308cafaf85aa45fa4616d6ba9319fe31
Pei-Han Jiang <peihan945@gmail.com>
2026-04-29 17:25:38 +0200

    set variable for output dir
    set variable for output dir instead of hard-coded

--- Lab07/lecture_material/web/build.sh
+++ Lab07/lecture_material/web/build.sh
@@ -2,12 +2,14 @@
 
 set -ueo pipefail
 
+html_dir="public"
+
 echo "<p>Version:" >version.inc.html
 git rev-parse --short HEAD >>version.inc.html 2>/dev/null || echo "unkown" >>version.inc.html
 echo "</p>" >>version.inc.html
 
-pandoc --template template.html -A version.inc.html index.md >public/index.html
-pandoc --template template.html -A version.inc.html rules.md >public/rules.html
-./table.py <score.csv | pandoc --template template.html --metadata title="Score" -A version.inc.html - >score.html
-cp main.css public/
+pandoc --template template.html -A version.inc.html index.md >"$html_dir/index.html"
+pandoc --template template.html -A version.inc.html rules.md >"$html_dir/rules.html"
+./table.py <score.csv | pandoc --template template.html --metadata title="Score" -A version.inc.html - >"$html_dir/score.html"
+cp main.css "$html_dir/"
 
